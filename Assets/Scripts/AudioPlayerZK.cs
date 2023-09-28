d0d75beab13ab5739082772f16f10f2f04224c38
Dean <119504721+Dean-zk@users.noreply.github.com>
2023-09-28 13:44:40 +0200

    added: audiofile script

---
+++ Assets/Scripts/AudioPlayerZK.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayerZK : MonoBehaviour
+{
+    AudioClip clips;
+    Camera cam;
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
