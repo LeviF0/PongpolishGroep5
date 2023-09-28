9571d2ee0eea5215708fb5fdea6b7894edcc7524
ElliotZeegers <0336362@student.rocvantwente.nl>
2023-09-28 14:50:40 +0200

    added: scripts

---
+++ Assets/Scripts/ScriptPlacer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPlacer : MonoBehaviour
+{
+    GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.Find("Ball").GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball != null)
+        {
+            print("ja");
+        }
+        else
+        {
+            print("nee");
+        }
+    }
+}
